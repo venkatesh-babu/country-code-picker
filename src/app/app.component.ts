e25be056fb17917477f6a95e1ba76d3e42d66d9b
babu <venkatesh@riota.in>
2024-06-30 00:02:10 +0530

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CountryCodePickerComponent } from '../../projects/country-code-picker/src/lib/country-code-picker/country-code-picker.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, CountryCodePickerComponent],
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   actionSelectEvent($event: any) {
